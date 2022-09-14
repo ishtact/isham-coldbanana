0599f2f795be86c7a7fcdc828bf7edf2cc6cefb4
Isham Sally <ishamtactical@gmail.com>
2022-09-14 23:55:26 +0400

    02: D1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http"; 
+
+interface Products {  
+  id: Number;  
+  product_name: String;  
+  image: String;  
+  price: String;  
+  stock_count: Number;  
+}  
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'Isham-ColdBanana';
+
+  public data:any = []
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(){
+    const url ='https://my-json-server.typicode.com/TomSearle/cb-devtest-api/products'
+    this.http.get(url).subscribe((res)=>{
+      this.data = res
+      console.log(this.data)
+    })
+  }
 }
